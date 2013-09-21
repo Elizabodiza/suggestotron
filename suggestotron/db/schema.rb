965298efa6d4b539b51c0ced033b9c9e9d94d559
Eliza Hale <ehale@college.harvard.edu>
2013-09-21 15:21:42 -0400

    Finished bootcamp, votes belong to users

--- suggestotron/db/schema.rb
+++ suggestotron/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921180821) do
+ActiveRecord::Schema.define(version: 20130921184943) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20130921180821) do
     t.integer  "topic_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
